1a2c1f52b91cb2696d3aada8d30303639f3494aa
Alejo Chavez <alejo.chavez@alumnos.frgp.utn.edu.ar>
2024-06-06 20:09:17 -0300

    Punto 2

--- Actvidad 2.4/Actividad2.4.sql
+++ Actvidad 2.4/Actividad2.4.sql
@@ -20,20 +20,29 @@ order by Datos_Personales.Nombres asc
 
 --Listado con apellidos y nombres de los usuarios que se hayan inscripto a cursos pero no hayan realizado ningún pago.
 
-SELECT DISTINCT * from Usuarios U
-inner join Inscripciones I on i.IDUsuario = u.ID
-inner join Datos_Personales DP on DP.ID = U.ID
+select DISTINCT dp.Nombres, dp.Apellidos from Datos_Personales DP
+inner join Usuarios U on U.ID = DP.ID
+left join Inscripciones I on I.IDUsuario = U.ID
 left join Pagos P on P.IDInscripcion = I.ID
-WHERE U.ID NOT IN
+WHERE U.ID NOT IN 
 (
-SELECT DISTINCT U.ID from Usuarios U
-inner join Datos_Personales DP on DP.ID = U.ID
-inner join Inscripciones I on i.IDUsuario = u.ID
+SELECT distinct u.ID FROM Datos_Personales DP
+inner join Usuarios U on U.ID = DP.ID
+inner join Inscripciones I on I.IDUsuario = U.ID
 inner join Pagos P on P.IDInscripcion = I.ID
-)
+) AND I.ID IS NOT NULL
 
-SELECT DISTINCT * from Usuarios U
-inner join Inscripciones I on i.IDUsuario = u.ID
-inner join Datos_Personales DP on DP.ID = U.ID
-left join Pagos P on P.IDInscripcion = I.ID
-ORDER BY DP.Apellidos, DP.Nombres DESC
+--query de toda las tablas
+SELECT * FROM Datos_Personales DP
+LEFT join Usuarios U on U.ID = DP.ID
+LEFT join Inscripciones I on I.IDUsuario = U.ID
+LEFT join Pagos P on P.IDInscripcion = I.ID
+
+--query de comprobacion
+select DP.Nombres, DP.Apellidos, COUNT(i.ID) as inscripciones, COUNT(p.id) as pagos FROM Datos_Personales DP
+LEFT join Usuarios U on U.ID = DP.ID
+LEFT join Inscripciones I on I.IDUsuario = U.ID
+LEFT join Pagos P on P.IDInscripcion = I.ID
+group by DP.Nombres, DP.Apellidos
+having COUNT(p.id) = 0
+
