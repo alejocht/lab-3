18192358a75a81b8165f192fb02d95cc409fc2b6
Alejo Chavez <alejo.chavez@alumnos.frgp.utn.edu.ar>
2024-06-09 15:39:09 -0300

    Update Actividad2.4.sql

--- Actvidad 2.4/Actividad2.4.sql
+++ Actvidad 2.4/Actividad2.4.sql
@@ -152,10 +152,157 @@ SELECT DISTINCT
 DP.Apellidos,
 DP.Nombres,
 U.NombreUsuario,
-() AS CursosPrincipiantes,
-() AS CursosAvanzados
+(SELECT COUNT(Cursos.IDNivel) FROM Usuarios
+LEFT JOIN Inscripciones ON Inscripciones.IDUsuario = Usuarios.ID
+LEFT JOIN Cursos ON Inscripciones.IDCurso = Cursos.ID
+INNER JOIN Niveles ON Niveles.ID = Cursos.IDNivel
+WHERE Niveles.Nombre LIKE 'Principiante' AND Usuarios.ID = U.ID)
+AS CursosPrincipiantes,
+(SELECT COUNT(Cursos.IDNivel) FROM Usuarios
+LEFT JOIN Inscripciones ON Inscripciones.IDUsuario = Usuarios.ID
+LEFT JOIN Cursos ON Inscripciones.IDCurso = Cursos.ID
+INNER JOIN Niveles ON Niveles.ID = Cursos.IDNivel
+WHERE Niveles.Nombre LIKE 'Avanzado' AND Usuarios.ID = U.ID)
+AS CursosAvanzados
 FROM Datos_Personales DP
 INNER JOIN Usuarios U ON DP.ID = U.ID
 LEFT JOIN Inscripciones I ON I.IDUsuario = U.ID
 LEFT JOIN Cursos C ON I.IDCurso = C.ID
 INNER JOIN Niveles N ON N.ID = C.IDNivel
+
+--Listado con nombre de los cursos y la recaudación de inscripciones de usuarios de género femenino que se inscribieron y la recaudación de inscripciones de usuarios de género masculino.
+
+SELECT DISTINCT
+C.Nombre,
+(SELECT
+SUM(Inscripciones.Costo)
+from Cursos 
+LEFT JOIN Inscripciones  on Inscripciones.IDCurso = Cursos.ID
+INNER JOIN Usuarios ON Usuarios.ID = Inscripciones.IDUsuario
+INNER JOIN Datos_Personales ON Datos_Personales.ID = Usuarios.ID
+WHERE Datos_Personales.Genero LIKE 'F' AND Cursos.ID = C.ID)
+as RecaudacionFemenina,
+(SELECT
+SUM(Inscripciones.Costo)
+from Cursos 
+LEFT JOIN Inscripciones  on Inscripciones.IDCurso = Cursos.ID
+INNER JOIN Usuarios ON Usuarios.ID = Inscripciones.IDUsuario
+INNER JOIN Datos_Personales ON Datos_Personales.ID = Usuarios.ID
+WHERE Datos_Personales.Genero LIKE 'M' AND Cursos.ID = C.ID)
+as RecaudacionMasculina
+from Cursos C
+LEFT JOIN Inscripciones I on I.IDCurso = C.ID
+LEFT JOIN Usuarios U ON U.ID = I.IDUsuario
+LEFT JOIN Datos_Personales DP ON DP.ID = U.ID
+--Listado con nombre de país de aquellos que hayan registrado más usuarios de género masculino que de género femenino.
+
+SELECT AUX.Nombre FROM
+(SELECT DISTINCT
+P.Nombre,
+(SELECT COUNT(Datos_Personales.ID) FROM Paises
+LEFT JOIN Localidades ON Localidades.IDPais = Paises.ID
+LEFT JOIN Datos_Personales ON Datos_Personales.IDLocalidad = Localidades.ID
+WHERE Datos_Personales.Genero LIKE 'M' AND Paises.ID = P.ID)
+as Masculinos,
+(SELECT COUNT(Datos_Personales.ID) FROM Paises
+LEFT JOIN Localidades ON Localidades.IDPais = Paises.ID
+LEFT JOIN Datos_Personales ON Datos_Personales.IDLocalidad = Localidades.ID
+WHERE Datos_Personales.Genero LIKE 'F' AND Paises.ID = P.ID)
+as Femeninos
+FROM Paises P
+LEFT JOIN Localidades L ON L.IDPais = P.ID
+LEFT JOIN Datos_Personales DP ON DP.IDLocalidad = L.ID) as AUX
+WHERE Masculinos > Femeninos
+
+--Listado con nombre de país de aquellos que hayan registrado más usuarios de género masculino que de género femenino pero que haya registrado al menos un usuario de género femenino.
+
+SELECT AUX.Nombre FROM
+(SELECT DISTINCT
+P.Nombre,
+(SELECT COUNT(Datos_Personales.ID) FROM Paises
+LEFT JOIN Localidades ON Localidades.IDPais = Paises.ID
+LEFT JOIN Datos_Personales ON Datos_Personales.IDLocalidad = Localidades.ID
+WHERE Datos_Personales.Genero LIKE 'M' AND Paises.ID = P.ID)
+as Masculinos,
+(SELECT COUNT(Datos_Personales.ID) FROM Paises
+LEFT JOIN Localidades ON Localidades.IDPais = Paises.ID
+LEFT JOIN Datos_Personales ON Datos_Personales.IDLocalidad = Localidades.ID
+WHERE Datos_Personales.Genero LIKE 'F' AND Paises.ID = P.ID)
+as Femeninos
+FROM Paises P
+LEFT JOIN Localidades L ON L.IDPais = P.ID
+LEFT JOIN Datos_Personales DP ON DP.IDLocalidad = L.ID) as AUX
+WHERE Masculinos > Femeninos AND Femeninos >= 1
+
+--Listado de cursos que hayan registrado la misma cantidad de idiomas de audio que de subtítulos.
+SELECT AUX.Nombre FROM
+(SELECT DISTINCT
+C.Nombre,
+(SELECT COUNT(Idiomas_x_Curso.IDFormatoIdioma) FROM Cursos
+LEFT JOIN Idiomas_x_Curso ON Idiomas_x_Curso.IDCurso = Cursos.ID
+LEFT JOIN FormatosIdioma ON FormatosIdioma.ID = Idiomas_x_Curso.IDFormatoIdioma
+LEFT JOIN Idiomas ON Idiomas.ID = Idiomas_x_Curso.IDIdioma
+WHERE FormatosIdioma.Nombre LIKE 'Audio' AND Cursos.ID = C.ID)
+AS Audio,
+(SELECT COUNT(Idiomas_x_Curso.IDFormatoIdioma) FROM Cursos
+LEFT JOIN Idiomas_x_Curso ON Idiomas_x_Curso.IDCurso = Cursos.ID
+LEFT JOIN FormatosIdioma ON FormatosIdioma.ID = Idiomas_x_Curso.IDFormatoIdioma
+LEFT JOIN Idiomas ON Idiomas.ID = Idiomas_x_Curso.IDIdioma
+WHERE FormatosIdioma.Nombre LIKE 'Subtitulo' AND Cursos.ID = C.ID)
+AS Subtitulo
+FROM Cursos C
+LEFT JOIN Idiomas_x_Curso IC ON IC.IDCurso = C.ID
+LEFT JOIN FormatosIdioma FI ON FI.ID = IC.IDFormatoIdioma
+LEFT JOIN Idiomas I ON I.ID = IC.IDIdioma) AS AUX
+WHERE AUX.Audio = AUX.Subtitulo
+
+--Listado de usuarios que hayan realizado más cursos en el año 2018 que en el 2019 y a su vez más cursos en el año 2019 que en el 2020.
+
+SELECT AUX.NombreUsuario FROM
+(SELECT DISTINCT
+U.NombreUsuario,
+(SELECT COUNT(Inscripciones.ID) 
+FROM Usuarios
+LEFT JOIN Inscripciones ON Inscripciones.IDUsuario = Usuarios.ID
+WHERE YEAR(Inscripciones.Fecha) = 2018 AND Usuarios.ID = U.ID)
+AS Cursos2018,
+(SELECT COUNT(Inscripciones.ID) 
+FROM Usuarios
+LEFT JOIN Inscripciones ON Inscripciones.IDUsuario = Usuarios.ID
+WHERE YEAR(Inscripciones.Fecha) = 2019 AND Usuarios.ID = U.ID)
+AS Cursos2019,
+(SELECT COUNT(Inscripciones.ID) 
+FROM Usuarios
+LEFT JOIN Inscripciones ON Inscripciones.IDUsuario = Usuarios.ID
+WHERE YEAR(Inscripciones.Fecha) = 2020 AND Usuarios.ID = U.ID)
+AS Cursos2020
+FROM Usuarios U
+LEFT JOIN Inscripciones I ON I.IDUsuario = U.ID) AS AUX
+WHERE AUX.Cursos2018 > AUX.Cursos2019 AND AUX.Cursos2019 > AUX.Cursos2020
+
+-- Listado de apellido y nombres de usuarios que hayan realizado cursos pero nunca se hayan certificado.
+-- Aclaración: Listado con apellidos y nombres de usuarios que hayan realizado al menos un curso y no se hayan certificado nunca.
+
+SELECT AUX.Apellidos, AUX.Nombres FROM
+(SELECT DISTINCT 
+DP.Apellidos,
+DP.Nombres,
+(select COUNT(Inscripciones.ID) from Usuarios
+LEFT JOIN Inscripciones ON Inscripciones.IDUsuario = Usuarios.ID
+WHERE Usuarios.ID = U.ID)
+AS Inscripciones,
+(select COUNT(Certificaciones.IDInscripcion) from Usuarios
+LEFT JOIN Inscripciones ON Inscripciones.IDUsuario = Usuarios.ID
+LEFT JOIN Certificaciones ON Certificaciones.IDInscripcion = Inscripciones.ID
+WHERE Usuarios.ID = U.ID)
+AS Certificaciones
+FROM Datos_Personales DP
+LEFT JOIN Usuarios U ON DP.ID = U.ID
+LEFT JOIN Inscripciones I ON I.IDUsuario = U.ID) AS AUX
+WHERE AUX.Certificaciones = 0 AND Inscripciones >= 1
+
+
+
+
+
+
