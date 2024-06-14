7a1423cb03af0e93afc906be8bf6339b47604747
Alejo Chavez <alejo.chavez@alumnos.frgp.utn.edu.ar>
2024-06-14 19:33:12 -0300

    Update Actividad3.1.sql

--- Actividad 3.1/Actividad3.1.sql
+++ Actividad 3.1/Actividad3.1.sql
@@ -122,4 +122,92 @@ begin
 	values (@Nombre, @CostoCurso, @CostoCertificacion, @Estreno, @DebeSerMayorDeEdad)
 end
 
-exec SP_NuevoCurso('Arte de la doma', 0, 5000, '2020-02-21', 0)
+EXEC SP_NuevoCurso 'Arte de la doma', 0, 5000, '2020-02-21', 0;
+
+--Hacer un procedimiento almacenado que permita modificar porcentualmente el Costo de Cursada de todos los cursos. 
+--El procedimiento debe recibir un valor numérico que representa el valor a aumentar porcentualmente. Por ejemplo, 
+--si recibe un 60. Debe aumentar un 60% todos los costos.
+
+create or alter procedure SP_AumentarCostos(@porcentaje float)
+as
+begin
+	set @porcentaje = 1 + @porcentaje/100
+	update Cursos
+	set CostoCertificacion = CostoCertificacion * @porcentaje,
+		CostoCurso = CostoCurso * @porcentaje
+end
+
+select * from Cursos
+
+exec SP_AumentarCostos 50;
+
+--Hacer un procedimiento almacenado llamado SP_PagarInscripcion que a partir de un IDInscripcion permita hacer un pago de la inscripción.
+--El pago puede ser menor al costo de la inscripción o bien el total del mismo.
+--El sistema no debe permitir que el usuario pueda abonar más dinero para una inscripción que el costo de la misma.
+--Se debe registrar en la tabla de pagos con la información que corresponda.
+
+create or alter procedure SP_PagarInscripcion (@IDInscripcion int , @importe money)
+as
+begin
+	declare @IDUsuario int
+	declare @costo money
+
+	select @IDUsuario = Inscripciones.IDUsuario from Inscripciones where Inscripciones.ID = @IDInscripcion
+	select @costo = Inscripciones.Costo from Inscripciones where Inscripciones.ID = @IDInscripcion
+
+	if (@importe > @costo)
+	begin
+		raiserror('EL PAGO NO PUEDE SER MAYOR AL COSTO DE INSCRIPCION', 16, 0)
+		return
+	end
+	else
+	begin
+		insert into Pagos (IDInscripcion, Fecha, Importe)
+		values (@IDInscripcion, GETDATE(), @importe)
+	end
+
+
+end
+
+exec SP_PagarInscripcion 2, 30000;
+select * from Pagos
+
+select Costo from Inscripciones where id = 1
+
+--Hacer un procedimiento almacenado llamado SP_InscribirUsuario que reciba un IDUsuario y un IDCurso y realice la inscripción a dicho curso de ese usuario.
+--El procedimiento debe realizar las siguientes comprobaciones:
+-- - El usuario no debe registrar deuda para poder inscribirse.
+-- - El usuario debe ser mayor de edad si el curso requiere esa condición.
+--En caso que estas comprobaciones sean satisfechas entonces registrar la inscripción.
+--Determinar el costo de la inscripción al valor actual del curso. Si alguna comprobación no se cumple, indicarlo con un mensaje de error correspondiente.
+
+create or alter procedure SP_InscribirUsuario(@IDUsuario int, @IDCurso int)
+as
+begin
+	declare @deuda money
+	declare @edad int
+	declare @mayordeedad bit
+	select @deuda = dbo.FN_DeudaxUsuario(@IDUsuario)
+	select @edad = dbo.FN_CalcularEdad(@IDUsuario)
+	select @mayordeedad = DebeSerMayorDeEdad from Cursos where ID = @IDCurso
+	
+	if (@deuda != 0)
+	begin
+		raiserror('LA DEUDA DEL USUARIO DEBE SER $0 PARA INSCRIBIRSE NUEVAMENTE',16,0)
+		return
+	end
+
+	if(@edad<18 AND @mayordeedad = 1)
+	begin
+		raiserror('EL USUARIO DEBE SER MAYOR DE EDAD PARA INGRESAR A ESTE CURSO',16,0)
+		return
+	end
+
+	declare @costoinscripcion money
+	select @costoinscripcion = CostoCurso from Cursos where ID = @IDCurso
+	insert into Inscripciones (IDUsuario,IDCurso,Fecha,Costo)
+	values(@IDUsuario, @IDCurso, GETDATE(), @costoinscripcion)
+
+end
+
+exec SP_InscribirUsuario 10, 10;
